5dfe7751132f97d265cb3f518b509278bcceabf9
nahom <nahomtendale@gmail.com>
2024-06-11 12:15:07 -0400

    started configuring auth with supabase

---
+++ app/login/page.tsx
@@ -0,0 +1,92 @@
+// supabase login
+import { login, signup } from './actions'
+// components
+import { Label } from "@/components/ui/label"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { LockIcon, MountainIcon } from '@/lib/customIcons'
+
+export default function LoginPage() {
+    return (
+        <div className="flex min-h-screen items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+            <div className="w-full max-w-md space-y-8">
+                <div>
+                    <MountainIcon className="mx-auto h-12 w-auto" />
+                    <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign in to your account</h2>
+                </div>
+                <form className="mt-8 space-y-6" action="#" method="POST">
+                    <div className="-space-y-px rounded-md shadow-sm">
+                        <div>
+                            <Label htmlFor="email" className="sr-only">
+                                Email address
+                            </Label>
+                            <Input
+                                id="email"
+                                name="email"
+                                type="email"
+                                autoComplete="email"
+                                required
+                                className="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                placeholder="Email address"
+                            />
+                        </div>
+                        <div>
+                            <Label htmlFor="password" className="sr-only">
+                                Password
+                            </Label>
+                            <Input
+                                id="password"
+                                name="password"
+                                type="password"
+                                autoComplete="current-password"
+                                required
+                                className="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                placeholder="Password"
+                            />
+                        </div>
+                    </div>
+                    <div className="flex items-center justify-between">
+                        <div className="flex items-center">
+                            <Input
+                                id="remember-me"
+                                name="remember-me"
+                                type="checkbox"
+                                className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                            />
+                            <Label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
+                                Remember me
+                            </Label>
+                        </div>
+                        <div className="text-sm">
+                            <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+                                Forgot your password?
+                            </a>
+                        </div>
+                    </div>
+                    <div>
+                        <Button
+                            formAction={login}
+                            className="group relative flex w-full justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        >
+                            <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                                <LockIcon className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" />
+                            </span>
+                            Sign in
+                        </Button>
+                    </div>
+                </form>
+                <div className="text-center">
+                    <span className="text-sm text-gray-500">Don't have an account?</span>
+                    <Button
+                        formAction={signup}
+                        variant="ghost"
+                        className="relative ml-2 inline-block text-sm font-medium text-indigo-600 hover:text-indigo-500"
+                    >
+                        Sign up
+                    </Button>
+
+                </div>
+            </div>
+        </div>
+    )
+}
