3510192afdc2a9f2d2cbc44afa3dc445772bd1ec
nahom <nahomtendale@gmail.com>
2024-06-13 11:41:03 -0400

    added profile dropdown on navbar with links to profile button to logout

--- components/authButton.jsx
+++ components/authButton.jsx
@@ -4,6 +4,14 @@ import { createClient } from '@/utils/supabase/server';
 import Link from 'next/link';
 import { redirect } from 'next/navigation';
 import { Button } from './ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem,
+} from '@/components/ui/dropdown-menu';
+import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar';
+import { LogOutIcon, UserIcon } from '@/lib/customIcons';
 
 export default async function AuthButton() {
   const supabase = createClient();
@@ -29,18 +37,47 @@ export default async function AuthButton() {
     await supabase.auth.signOut();
     return redirect('/login');
   };
+
   function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
+
   return user ? (
     <div className="flex items-center gap-4">
       <div>
-      Hey,
-      <span className='font-bold'> {capitalizeFirstLetter(userName)}</span>!
+        Hey,
+        <span className="font-bold"> {capitalizeFirstLetter(userName)}</span>!
       </div>
-      <form action={signOut}>
-        <Button variant="destructive">Logout</Button>
-      </form>
+      <div className="ml-auto flex gap-2">
+        <DropdownMenu modal={false}>
+          <DropdownMenuTrigger asChild className="cursor-pointer">
+            <Avatar className="h-9 w-9">
+              <AvatarImage src="/images/app/placeholder-user.jpg" />
+              <AvatarFallback>JP</AvatarFallback>
+              <span className="sr-only">Toggle user menu</span>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem>
+              <Link href="/user/my-profile" prefetch={false} className="flex">
+                <UserIcon className="mr-2 h-4 w-4" stroke="#030816" />
+                Profile
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <form action={signOut}>
+                <button type="submit" className="flex cursor-pointer">
+                  <LogOutIcon className="mr-2 h-4 w-4" />
+                  Logout
+                </button>
+              </form>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <Link href="/user" prefetch={false} variant="secondary">
+          <Button variant="secondary">Dashboard</Button>
+        </Link>
+      </div>
     </div>
   ) : (
     <Link
