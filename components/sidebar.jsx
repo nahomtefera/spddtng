03078adc54dade6f4abcb6461c1b752e51fddda0
nahom <nahomtendale@gmail.com>
2024-06-15 12:49:41 -0400

    added code to make sidebar appear collapsed initially

--- components/sidebar.jsx
+++ components/sidebar.jsx
@@ -19,11 +19,16 @@ import Image from 'next/image';
 export default function Sidebar({ links, isAdminDashboard, isUserDashboard }) {
   const pathname = usePathname();
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(false);
+  const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(true);
   const [userName, setUserName] = useState('');
   const [userProfilePicture, setUserProfilePicture] = useState('/images/app/placeholder-user.jpg');
 
   const isActive = (path) => pathname === path;
+
+  const handleToggleSidebar = (bool) => {
+    bool && setIsSidebarCollapsed(false);
+    setIsSidebarOpen(bool);
+  }
 
   useEffect(() => {
     async function fetchUserProfile() {
@@ -59,7 +64,7 @@ export default function Sidebar({ links, isAdminDashboard, isUserDashboard }) {
       {/* Toggle Sidebar button */}
       <div className="relative">
         <button
-          onClick={() => setIsSidebarOpen(true)}
+          onClick={() => handleToggleSidebar(true)}
           className="fixed md:hidden top-6 right-8 z-50 flex h-10 w-10 items-center justify-center rounded-md bg-gray-900 text-gray-50 transition-colors hover:bg-gray-900/80 focus:outline-none focus:ring-2 focus:ring-gray-950 focus:ring-offset-2 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/80 dark:focus:ring-gray-300"
         >
           <MenuIcon className="h-6 w-6" />
@@ -78,7 +83,7 @@ export default function Sidebar({ links, isAdminDashboard, isUserDashboard }) {
           variant="ghost"
           size="icon"
           className="bg-gray-900 rounded md:hidden absolute top-4 right-4"
-          onClick={() => setIsSidebarOpen(false)}
+          onClick={() => handleToggleSidebar(false)}
         >
           <XIcon className="w-8 h-8" />
         </Button>
@@ -138,7 +143,7 @@ export default function Sidebar({ links, isAdminDashboard, isUserDashboard }) {
               key={link.href}
               href={link.href}
               className={`flex items-center gap-2 px-3 py-2 rounded-md hover:bg-[#202135] transition-colors ${isActive(link.href) && 'bg-[#202135]'}`}
-              onClick={() => setIsSidebarOpen(false)}
+              onClick={() => handleToggleSidebar(false)}
               prefetch={link.prefetch}
             >
               {link.icon}
