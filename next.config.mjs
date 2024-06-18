e13a1fffe20bfed7941dd4488873f7a08418cc13
nahom <nahomtendale@gmail.com>
2024-06-18 17:57:25 -0400

    modified next config to allow images from eventbrite

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    compress: true,
-    async headers() {
-      return [
-        {
-          // Match all static files with specific extensions
-          source: '/:all*(svg|jpg|png|css|js)',
-          headers: [
-            {
-              key: 'Expires',
-              value: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000).toUTCString(), // Set Expires header to 30 days in the future
-            },
-          ],
-        },
-      ];
-    },
-    images: {
-      domains: ['img.evbuc.com'],
-    }
-  };
-    
+  compress: true,
+  async headers() {
+    return [
+      {
+        // Match all static files with specific extensions
+        source: '/:all*(svg|jpg|png|css|js)',
+        headers: [
+          {
+            key: 'Expires',
+            value: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000).toUTCString(), // Set Expires header to 30 days in the future
+          }
+        ]
+      },
+      
+    ];
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.evbuc.com',
+        port: '',
+        pathname: '/**', // Adjust this pattern according to your specific path structure
+      },
+    ],
+  }
+};
 
-export default nextConfig;
+export default nextConfig; // Using ES module syntax
